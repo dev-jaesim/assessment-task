dcdda7f7bf13fbd617ebf7bbb519f5d1e7c1d55a
jaensim <jaeneung.sim@customerservice.nsw.gov.au>
2023-09-22 16:45:06 +1000

    Added a lambda

--- lib/jae-task-stack.ts
+++ lib/jae-task-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class JaeTaskStack extends cdk.Stack {
+  private fetchFeatureCollection: NodejsFunction;
+  private fetchFeatureCollectionUrl: lambda.FunctionUrl;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.createLambda();
+    this.createLambdaUrl();
+  }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'JaeTaskQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  private createLambda() {
+    this.fetchFeatureCollection = new NodejsFunction(this, 'FetchFeatureCollectionLambda', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: './lib/lambda/index.js',
+      handler: 'handler'
+    });
+  }
+
+  private createLambdaUrl() {
+    this.fetchFeatureCollectionUrl = this.fetchFeatureCollection.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE
+    });
+
+    new cdk.CfnOutput(this, 'FunctionUrl ', { value: this.fetchFeatureCollectionUrl.url });
   }
 }
